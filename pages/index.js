437f1719aadf475e7df1dcdfc58ebec241ff8b13
Gershwen <76742941+Gershwen@users.noreply.github.com>
2021-12-17 14:14:16 +0200

    My 1st Next.js app commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+//the Layout component gets imported and used to wrap around the page's content
+import Layout from "../components/MyLayout";
+
+const Home = ()=>{
+    return (
+        <Layout>
+            <h1>Web Developement Portfolio of Gershwen McPherson</h1>
+        </Layout>
+    )
+}
+
+export default Home;
---
+++ readMe.md
@@ -0,0 +1,3 @@
+# My 1st website built with Next.js
+
+## This is my portfolio site that points to the github repositories of my projects  
